96fd0f98165eef6fc609e1dcbeffed672846bc92
Ryan Vine <R.vine66@gmail.com>
2022-03-31 14:44:20 +0100

    Added first variables & functions for calculations

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 
-//Button section
+//Button class section
 const button = document.getElementsByClassName("calculator__button");
 const numbers = document.querySelectorAll(".calculator__number");
-const equals = document.querySelector("#equals");
 const operators = document.querySelectorAll(".calculator_operator");
 const display = document.querySelector(".calculator__display")
 
+//individual button section
+
+const clear = document.querySelector(".calculator__clear");
+const plusMinus = document.querySelector(".calculator__plus-or-minus")
+const percent = document.querySelector("calculator__percent");
+const decimal = document.querySelector(".calculator__button-decimal");
+const equals = document.querySelector("#equals");
+
+//More specific variables
+
+
+//Functions
+
+const numberClick = (num) => {
+  console.log(num)
+
+}
+
+clear.addEventListener("click", () => {
+  display.innerHTML = "";
+})
+
 // create a variable for each button loop - eventlisten - onclick
-console.log(numbers[0]);
 //print buttons to console 
+
+
+
 
 numbers.forEach(number => {
   number.addEventListener("click", () => {
-    console.log(number)
+    console.log(number);
+    if (display.innerHTML === '0') {
+      display.innerHTML = number.innerHTML
+    } else {
+      display.innerHTML += number.innerHTML;
+    }
   })
-  number.appendChild(display);
 })
